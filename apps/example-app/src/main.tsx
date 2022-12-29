74134b9a0405829163b3302717231b0adca051da
Adam Goldman <goldy@adamgoldman.me>
2022-12-29 14:06:53 +0200

    repro

--- apps/example-app/src/main.tsx
+++ apps/example-app/src/main.tsx
@@ -1,7 +1,10 @@
+import { myLib } from '@nx-graph-bug/my-lib';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
+
+console.log(myLib());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
